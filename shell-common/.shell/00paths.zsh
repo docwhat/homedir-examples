ab316a84aafae093e8c0fd221b8bd80f3e2b7006
Christian Höltje <docwhat@gerf.org>
2011-10-16 03:03:08 -0400

    Sped up zsh pathman

--- shell-common/.shell/00paths.zsh
+++ shell-common/.shell/00paths.zsh
@@ -1,73 +1,69 @@
 ## -*- sh -*-
 
 function pathman {
-    setopt extendedglob
-    local append=false
-    local force=false
-    local -aU new_paths
-    new_paths=()
+  setopt extendedglob
+  local append=false
+  local -aU new_paths
+  new_paths=()
 
-    PATH="${PATH:-}"
-    MANPATH="${MANPATH:-}"
+  PATH="${PATH:-}"
+  MANPATH="${MANPATH:-}"
 
-    while (( $# > 0 )); do
-	case "$1" in
-	    -f|--force) force=true ;;
-	    -a|--append) append=true ;;
-	    -*)
-		echo "Invalid flag: $1"
-		return 1
-		;;
-	    *)
-		new_paths=( "$1" $new_paths[*])
-		;;
-	esac
-	shift
+  if [[ $# = 0 ]]; then
+    echo "Usage: $0 [-a|--append] DIR [DIR...]"
+    return 0
+  fi
+
+  while (( $# > 0 )); do
+    case "$1" in
+      -f|--force) ;; # Compatability, ignored
+      -a|--append) append=true ;;
+      -h|--help) pathman ; return 0 ;;
+      -*)
+      echo "Invalid flag: $1"
+      return 1
+      ;;
+      *)
+      new_paths=( "$1" $new_paths[*])
+      ;;
+    esac
+    shift
+  done
+
+  ## add to PATH
+  local new_path
+  for new_path in "${new_paths[@]}"; do
+
+    if [[ -d "${new_path}" ]]; then
+      if [[ "${append}" != true ]]; then
+        # Remove from path
+        path=("${(@)path:#$new_path}")
+      fi
+      # Add the new_path
+      if [[ "${append}" = true ]]; then
+        path+=("${new_path}")
+      else
+        PATH="${new_path}:${PATH}"
+      fi
+    fi
+    for mandir in "${new_path/%\/[^\/]##\/#//man}" "${new_path/%\/[^\/]##\/#//share/man}"; do
+      if [[ -d "${mandir}" ]]; then
+        if [[ "${append}" != true ]]; then
+          # Remove from manpath
+          manpath=("${(@)manpath:#$new_path}")
+        fi
+        # Add the new_path
+        if [[ "${append}" = true ]]; then
+          manpath+=("${mandir}")
+        else
+          MANPATH="${mandir}:${MANPATH}"
+        fi
+      fi
     done
+  done
 
-    ## add to PATH
-    local new_path
-    for new_path in "${new_paths[@]}"; do
-	if [[ -d "${new_path}" ]]; then
-	    if [[ "${force}" = true ]]; then
-	        # Remove any existing versions of new_path
-		local -a tmp
-		path=("${(@)path:#${new_path}}")
-	    elif [[ ${path[(i)${new_path}]} -le ${#path} ]]; then
-		# Skip, it already exists
-		continue
-	    fi
-	    # Add the new_path
-	    if [[ -z "${PATH}" ]]; then
-		PATH="${new_path}"
-	    elif [[ "${append}" = true ]]; then
-		PATH="${PATH}:${new_path}"
-	    else
-		PATH="${new_path}:${PATH}"
-	    fi
-	fi
-	for mandir in "${new_path/%\/[^\/]##\/#//man}" "${new_path/%\/[^\/]##\/#//share/man}"; do
-	    if [[ -d "${mandir}" ]]; then
-		if [[ "${force}" = true ]]; then
-    	            # Remove any existing versions of mandir
-		    manpath=("${(@)manpath:#${mandir}}")
-		elif [[ ${manpath[(i)${mandir}]} -le ${#manpath} ]]; then
-		    # Skip, it already exists
-		    continue
-		fi
-	        # Add the new_path
-		if [[ -z "${MANPATH}" ]]; then
-		    MANPATH="${manpath}"
-		elif [[ "${append}" = true ]]; then
-		    MANPATH="${MANPATH}:${mandir}"
-		else
-		    MANPATH="${mandir}:${MANPATH}"
-		fi
-	    fi
-	done
-    done
-
-    export PATH MANPATH
+  typeset -U path manpath
+  export PATH MANPATH
 }
 
 # EOF
